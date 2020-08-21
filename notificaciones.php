16096a5d0fb0da55087aeff391d778d6638181e4
julian <rodriguezpetzjulian@gmail.com>
2020-08-21 12:39:43 -0300

    caca

--- notificaciones.php
+++ notificaciones.php
@@ -1,39 +1,24 @@
 Recibiendo notificaciones
 
 <?php
+   http_response_code(200);
 
 
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $myfile = fopen("info1.txt", "w") or die("Unable to open file!");
-        /*
-        $data = array(
-            "id" => $_POST['id'],
-            "type" => $_POST['type']
-        );
-        $txt = json_encode($data);
-        */
-        $txt = file_get_contents('php://input');
-        fwrite($myfile, $txt);
-        
-        fclose($myfile);
-        
-        $myfile2 = fopen("info2.txt", "w") or die("Unable to open file!");
-        $txt2 = "Entro al POST y paso despues de escribir el primer archivo.";
-        fwrite($myfile2, $txt2);
-        fclose($myfile2);
-    }
-    else if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-        $myfile = fopen("info2.txt", "w") or die("Unable to open file!");
-        $txt = json_encode($_GET);
-        fwrite($myfile, $txt);
-        fclose($myfile);
-    }
+   if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+       $myfile = fopen("notificacion.txt", "w+") or die("Unable to open file!");
+       
+       $txt = file_get_contents('php://input');
+       fwrite($myfile, $txt);
+       
+       fclose($myfile);
+
+   }
+   else if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+       $myfile = fopen("notificacion.txt", "r") or die("No se puede leer el archivo!");
+
+       $contenido=fread($myfile,filesize($myfile));
+       var_dump($contenido);
+       fclose($myfile);
+   }
     
-    $myfile = fopen("info.txt", "w") or die("Unable to open file!");
-    $txt = json_encode($_REQUEST);
-    fwrite($myfile, $txt);
-    fclose($myfile);
-    
-    http_response_code(200);
-    
-    ?>
+?>
