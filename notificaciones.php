bba4c4efb881716e260a863cca53e560f8640df2
julian <rodriguezpetzjulian@gmail.com>
2020-08-21 12:07:33 -0300

    notificaciones 2

--- notificaciones.php
+++ notificaciones.php
@@ -3,23 +3,27 @@ Recibiendo notificaciones
 <?php
     http_response_code(200);
 
-    // SDK de Mercado Pago
-    require __DIR__ .  '/vendor/autoload.php';
 
-    // Agrega credenciales
-    MercadoPago\SDK::setAccessToken('APP_USR-894658496562814-061920-4c4e8dedca24e63d3e92cda4c1e5df8e-562339626');
-
-    try {
-        //code...
-        mail(
-            "rodriguezpetzjulian@gmail.com",
-            "json",
-            json_encode($_POST)
-        );
-        echo("Envie el mail");
-    } catch (\Throwable $th) {
-        echo("Ocurrio un error");
-        var_dump($th);
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $myfile = fopen("notificacion.txt", "w+") or die("Unable to open file!");
+        
+        //$txt = file_get_contents('php://input');
+        $json=json_encode($_POST);
+        fwrite($myfile, $json);
+        
+        fclose($myfile);
+        
+        //$myfile2 = fopen("info2.txt", "w") or die("Unable to open file!");
+        //$txt2 = "Entro al POST y paso despues de escribir el primer archivo.";
+        //fwrite($myfile2, $txt2);
+        //fclose($myfile2);
+    }
+    else if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        $myfile = fopen("notificacion.txt", "r") or die("No se puede leer el archivo!");
+        //$txt = json_encode($_GET);
+        $contenido=fread($myfile,filesize($myfile));
+        var_dump($contenido);
+        fclose($myfile);
     }
 
 ?>
