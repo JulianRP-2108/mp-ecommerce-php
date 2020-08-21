ee954bcf0ae73280b9d3603274a9616ab1268f6e
julian <rodriguezpetzjulian@gmail.com>
2020-08-21 11:17:44 -0300

    notificaciones1

--- notificaciones.php
+++ notificaciones.php
@@ -1,1 +1,24 @@
 Recibiendo notificaciones
+
+<?php
+    http_response_code(200);
+
+    // SDK de Mercado Pago
+    require __DIR__ .  '/vendor/autoload.php';
+
+    // Agrega credenciales
+    MercadoPago\SDK::setAccessToken('APP_USR-894658496562814-061920-4c4e8dedca24e63d3e92cda4c1e5df8e-562339626');
+
+    try {
+        //code...
+        mail(
+            "rodriguezpetzjulian@gmail.com",
+            "json",
+            json_encode($_POST)
+        );
+    } catch (\Throwable $th) {
+        echo("Ocurrio un error");
+        var_dump($th);
+    }
+
+?>
